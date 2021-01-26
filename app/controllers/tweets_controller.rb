4e5e5d24e2f54967a519aaacbd9a1db6c08cb661
yokoe9073 <yokoe9073@gmail.com>
2021-01-27 06:31:15 +0900

    ストロングパラメーター設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,5 +6,14 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
   
 end
