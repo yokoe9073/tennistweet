6f8893222b59b02c1aaa54538610f46b0eeba06f
yokoe9073 <yokoe9073@gmail.com>
2021-01-29 13:26:59 +0900

    降順に並び替える

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :show]
   def index
-    @tweets = Tweet.includes(:user)
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
 
   def new
