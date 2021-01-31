5d2a43b575c38e7dc87f57e251689c77d62b7e83
yokoe9073 <yokoe9073@gmail.com>
2021-01-31 14:56:21 +0900

    Revert "commentモデル作成　migrate"
    This reverts commit 26676bf2dd8c4f3210b5e8204fa15dc0b4a6e9fb.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_091438) do
-
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "tweet_id"
-    t.text "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_01_27_100153) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text"
